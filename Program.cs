28da614e6abb2d5d633644f63d6653128548caff
Robert Tublen <robert.tublen@educado.se>
2020-10-07 14:37:55 +0200

    Change message to "Hola el mundo"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Sunfleet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola el mundo!");
         }
     }
 }
